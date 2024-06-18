eb3e1a0fb0f7600e1fa3dce7cd9bea5325fbf7fa
Koushal Kumar <koushalkumar2224@gmail.com>
2024-06-18 23:00:22 +0530

    Deployment

---
+++ src/main/java/com/SpringAPI/Login_SignUp/LoginSignUpApplication.java
@@ -0,0 +1,13 @@
+package com.SpringAPI.Login_SignUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSignUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSignUpApplication.class, args);
+	}
+
+}
